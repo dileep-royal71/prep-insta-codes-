730232d7b64be9db78016ce135a7c5073ffcdf47
Dileep <vandhavasidileep@gmail.com>
2025-07-13 14:50:18 +0530

    Add files via upload

---
+++ prep code-1.py
@@ -0,0 +1,7 @@
+#aim:-Positive or Negative number
+"""---program starts here----"""
+a=int(input("enter number to check whether postive or negative :"))
+if a>=10:
+    print("postive")
+else:
+    print("Negative")
